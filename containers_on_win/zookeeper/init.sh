9413965c8b1d44650b8933ef991070ff67492515
zzjhacn <long1795@gmail.com>
2017-07-14 10:35:56 +0800

    sync

---
+++ containers_on_win/zookeeper/init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir -p /opt/docker/data/zookeeper/zoo1/data
+mkdir -p /opt/docker/data/zookeeper/zoo1/datalog
+mkdir -p /opt/docker/data/zookeeper/zoo2/data
+mkdir -p /opt/docker/data/zookeeper/zoo2/datalog
+mkdir -p /opt/docker/data/zookeeper/zoo3/data
+mkdir -p /opt/docker/data/zookeeper/zoo3/datalog
